38247b2c8cd0fb8a337ee2fb7e055b7ce969f85a
awillisford <64561892+awillisford@users.noreply.github.com>
2020-05-17 02:50:36 -0400

    Added user-defined obstacles drawing
    After setting start and end point, the user may paint obstacles on the screen that the algorithm will eventually avoid.

--- path_game.py
+++ path_game.py
@@ -5,6 +5,7 @@ import math
 white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
+black = (0, 0, 0)
 
 # Initialize pygame
 pygame.init()
@@ -19,7 +20,7 @@ size = 40
 pygame.display.set_caption("Path-Finding Algorithm")
 pygame.display.set_icon(pygame.image.load('maze.png'))
 
-# Two dimensional array of 0s
+# Two dimensional array of 0s - used for defining color of grid squares
 array = [[0] * 25 for x in range(16)]  # 25 columns - 16 rows
 
 num_columns = 25
@@ -35,6 +36,8 @@ def grid():
                 color = green
             elif array[row][column] == 2:
                 color = red
+            elif array[row][column] == 3:
+                color = black
             square = pygame.Rect(column * (size + 1), row * (1 + size), size, size)
             pygame.draw.rect(screen, color, square)
 
@@ -61,21 +64,36 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         elif event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            x, y = mouse_pos
+            x = event.pos[0]
+            y = event.pos[1]
             column, row = find_grid_position(x, y)
-            print('Grid position:', column, row)
-            if any(2 in x for x in array):                # if any square is red, don't color any more squares
+            if any(2 in pos for pos in array):                # if any square is red, don't color any more squares
                 pass
-            elif any(1 in x for x in array):
+            elif any(1 in pos for pos in array):
                 if array[row - 1][column - 1] != 1:       # if not already green, turn red
                     array[row - 1][column - 1] = 2
                     grid()
+                    print('Grid position:', column, row)
                     print('- End point selected')
-            elif any(1 in x for x in array)not in array:  # if no squares green, turn green
+            elif any(1 in pos for pos in array)not in array:  # if no squares green, turn green
                 array[row - 1][column - 1] = 1
                 grid()
+                print('Grid position:', column, row)
                 print('- Start point selected')
+        # paint obstacles by holding mouse down
+        if pygame.mouse.get_pressed()[0]:
+            x = event.pos[0]
+            y = event.pos[1]
+            column, row = find_grid_position(x, y)
+            if any(2 in pos for pos in array):
+                # if end point already selected
+                if any(2 in pos for pos in array):
+                    # if not already green or red, turn black
+                    if array[row - 1][column - 1] != 1 and array[row - 1][column - 1] != 2 and array[row - 1][column - 1] != 3:
+                        array[row - 1][column - 1] = 3
+                        grid()
+                        print('Grid position:', column, row)
+                        print('- Obstacle placed')
 
     pygame.display.update()
     clock.tick(30)
